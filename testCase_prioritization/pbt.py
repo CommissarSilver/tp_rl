aaa9be4198c20ea4cd23925b3ebe08228d88a483
CommissarSilver <commissarsilver@gmail.com>
2022-01-01 14:21:46 -0500

    added a test that didn't work. will delete later

--- testCase_prioritization/pbt.py
+++ testCase_prioritization/pbt.py
@@ -1,4 +1,7 @@
 import this
+import TPDRL
+import TPAgentUtil
+
 # !!! OK BROTHER. HERE'S HOW IT SHOULD GO:
 #   1 - TPDRL.py -> experiment function (line 50):
 #           This function creates the environment and the agent's model.
@@ -13,3 +16,40 @@ import this
 #           stable-baselines has a function that allows modifying and accessing each model's parameters.
 #           we need to access model parameters for copying the weights. The hyperparams are going to be another story.
 # !!! The documetation says MODEL PARAMETERS. I don't know if it means weights or not. If not, then we just have to save and load models.
+
+
+# The problem with this is that it is returning everything as a method. I don't think it is possible to change a functions variables.
+
+
+class Member:
+    """[summary]
+
+    Args:
+        TPAgentUtil ([type]): [description]
+    """
+
+    def __init__(self, algo, env) -> None:
+        super().__init__()
+        agent_model = TPAgentUtil.TPAgentUtil.create_model(algo=algo, env=env)
+        # !!! Hyperparameter as input to the algorithm
+        self.model = agent_model  # this line is useless
+
+    def mutate(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def get_agent_params(self):
+        agent_params = self.model.get_parameters()
+        return agent_params
+
+
+class Population:
+    def __init__(
+        self, population_size: int, population_algo: str, population_mode: str
+    ) -> None:
+        pass
+
+def test():
+    def another_test()
